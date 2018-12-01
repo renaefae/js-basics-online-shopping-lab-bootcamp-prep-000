78fb4f4bd0bce4faa4e2c312291e50b56667df9c
Naenae L <oneprincesslea@gmail.com>
2018-12-01 02:31:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,20 +5,43 @@ function getCart() {
 }
 
 function setCart(c) {
-  cart = c;
-  return cart;
+  cart = c; 
+  return cart; 
 }
 
-function addToCart(item) {
+function addToCart(item) {  
  // write your code here
-}
+  var itemObject= {
+  itemName: item, 
+  itemPrice: Math.floor(Math.random()*100)+1, 
+  }; 
+  
+  cart.push(itemObject); 
+   
+  return `${this.itemName} has been added to your cart.`; 
+} 
+
+var messageAboutCart = addToCart("apples", "kiwis");
+
+console.log(cart.length); 
+console.log(cart); 
 
 function viewCart() {
-  // write your code here
+  // write your code here 
+  if (cart.length===0){
+    return "Your cart be empty."; 
+  } else { 
+  var  cartLength = cart.length; 
+  for (i=0; i<= cartLength; i++){
+  var stuffInCart = `In your cart, you have ${cart[0].itemName}.`  
+  return  stuffInCart; } } 
 }
+
+console.log(viewCart());  
 
 function total() {
   // write your code here
+  return itemsInCart[0]+itemsInCart[1] ; 
 }
 
 function removeFromCart(item) {
@@ -28,3 +51,4 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
 }
+
